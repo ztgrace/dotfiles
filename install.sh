4d35a0fc6d6a3ec4de26478672209d22924f9813
Zach Grace <ztgrace@gmail.com>
2015-12-30 16:13:56 -0600

    Adding inputrc

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ git submodule update --init --recursive
 ln -is $CWD/.bashrc $HOME
 ln -is $CWD/.dir_colors $HOME
 ln -is $CWD/.gdbinit $HOME
+ln -is $CWD/.inputrc $HOME
 ln -is $CWD/.net-stats.sh $HOME
 ln -is $CWD/.tmux.conf $HOME
 ln -is $CWD/.vim $HOME
